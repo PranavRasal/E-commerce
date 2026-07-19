5536aea925b50e31a4a4025a9f91e9275496f618
PranavRasal <pranavcode19@gmail.com>
2026-07-19 23:47:19 +0530

    feat: add admin middleware and cloudinary configuration

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,15 @@
+import cloudinary from 'cloudinary';
+import dotenv from 'dotenv';
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
+
+CLOUDINARY_CLOUD_NAME
+CLOUDINARY_API_KEY
+CLOUDINARY_API_SECRET
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { registerUser , loginUser , getAllUsers } from '../controllers/authController.js';
 import protect from '../middleware/authmiddleware.js';
+import admin from '../middleware/adminMiddleware.js';
 
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/users', protect, getAllUsers); // for admin only, you can add role-based authorization later
+router.get('/users', protect , admin , getAllUsers); // for admin only, you can add role-based authorization later
 
+export default router;
