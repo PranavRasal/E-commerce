2ca307b172a00d368b31c54e9642e98d0398433d
PranavRasal <pranavcode19@gmail.com>
2026-07-22 23:40:52 +0530

    feat: implement deleteProduct function to remove a product by ID

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -49,3 +49,18 @@ export const getProductById = async (req , res) =>{
         res.status(500).json({ message: error.message });
     }
 }
+
+//remove product by ID
+export const deleteProduct = async (req , res) =>{
+    try{
+        const product = await Product.findById(req.params.id) ;
+        if(!product){
+            return res.status(404).json({message : "Product not found"});
+        }
+        await product.remove();
+        res.status(200).json({message : "Product deleted successfully"});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
