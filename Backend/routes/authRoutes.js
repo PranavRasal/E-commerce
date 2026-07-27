6f068295688576459835631c3d84f9331b50206d
PranavRasal <pranavcode19@gmail.com>
2026-07-27 23:42:37 +0530

    fix: add comment to clarify public route for user registration

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -6,7 +6,7 @@ import admin from '../middleware/adminMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', registerUser);// Public route for user registration
 router.post('/login', loginUser); // Public route for user login
 router.get('/users', protect , admin , getAllUsers); // for admin only, you can add role-based authorization later
 
