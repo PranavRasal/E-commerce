4856ea78173b142b5bd20e5883294381b57a7ee8
PranavRasal <pranavcode19@gmail.com>
2026-08-01 00:40:07 +0530

    feat: add payment verification logic and analytics data retrieval

---
+++ Backend/controllers/analyticsController.js
@@ -0,0 +1,26 @@
+import Order from '../models/order.model.js';
+import Product from '../models/product.model.js';
+import User from '../models/user.model.js';
+
+const getAnalyticsData = async (req, res) => {
+  try {
+    const totalOrders = await Order.countDocuments({}); // Count all orders in the database
+    const totalUsers = await User.countDocuments({role : 'user'}); // Count all users in the database
+    const totalProducts = await Product.countDocuments({}); // Count all products in the database
+
+    const orders = await Order.find({}) ; 
+
+    const totalRevenue = orders.reduce((acc, order) => acc + order.totalAmount, 0);
+     // Calculate total revenue from all orders
+
+
+
+    // Analytics data retrieval logic would go here
+    res.status(200).json({totalUsers, totalOrders,  totalProducts , totalRevenue  });
+  } catch (error) {
+    console.error('Error retrieving analytics data:', error);
+    res.status(500).json({ error: 'Failed to retrieve analytics data' });
+  }
+};
+
+export { getAnalyticsData };
--- Backend/routes/analyticsRoutes.js
+++ Backend/routes/analyticsRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-;
+import protect from '../middleware/authmiddleware.js';
+import admin from '../middleware/adminMiddleware.js';
+import { getAnalyticsData } from '../controllers/analyticsController.js';
 
 
 const router = express.Router();
 
+router.get('/', protect, admin, getAnalyticsData); // Admin-only route for analytics data
 
 export default router;
