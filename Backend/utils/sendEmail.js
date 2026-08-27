d3ea827629f74c767b930af37aba1e1860d281a1
PranavRasal <pranavcode19@gmail.com>
2026-08-27 16:40:21 +0530

    fix: update email transport configuration to use SMTP settings for Gmail

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -2,7 +2,10 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (to , subject , message) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000,
